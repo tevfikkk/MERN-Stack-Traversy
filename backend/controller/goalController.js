d9f6af7f0780e273fbed4709a4e9efc83ba7512c
Tevfik Hacıhamdioğlu <tevfikhoglu@gmail.com>
2022-03-01 19:02:32 +0300

    Goals controller and routes

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,30 +1,37 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
-const getGoals = (req, res) => {
-  res.status(200).json({ messeage: 'Get goals' })
-}
+const getGoals = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Get goals' })
+})
 
 // @desc Set a goal
 // @route POST /api/goals
 // @access Private
-const setGoal = (req, res) => {
-  res.status(200).json({ messeage: 'Set a goal' })
-}
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400)
+    throw new Error('Please add a text field')
+  }
+
+  res.status(200).json({ message: 'Set a goal' })
+})
 
 // @desc Update a goal
 // @route PUT /api/goals
 // @access Private
-const updateGoal = (req, res) => {
-  res.status(200).json({ messeage: `Update goal ${req.params.id}` })
-}
+const updateGoal = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` })
+})
 
 // @desc Delete a goal
 // @route DELETE /api/goals
 // @access Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` })
-}
+})
 
 module.exports = {
   getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 const app = express()
 
+// middleware
+app.use(express.json()) // provides reading requests as a json type
+app.use(express.urlencoded({ extended: false }))
+
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler) // overwrites the default Express Error Handler
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
